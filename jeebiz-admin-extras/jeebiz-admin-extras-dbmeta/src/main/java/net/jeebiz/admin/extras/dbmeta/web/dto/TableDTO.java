48d6cc3c11a440a4aabdbc2227de316e2b75dcd0
wandl <hnxxyhcwdl1003@163.com>
2021-01-05 22:51:24 +0800

    修改

--- jeebiz-admin-extras/jeebiz-admin-extras-dbmeta/src/main/java/net/jeebiz/admin/extras/dbmeta/web/dto/TableDTO.java
+++ jeebiz-admin-extras/jeebiz-admin-extras-dbmeta/src/main/java/net/jeebiz/admin/extras/dbmeta/web/dto/TableDTO.java
@@ -1,36 +1,27 @@
 package net.jeebiz.admin.extras.dbmeta.web.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel(value = "TableDTO", description = "表基本信息DTO")
+@Data
 public class TableDTO {
 
 	/**
 	 * 表名
 	 */
+	@ApiModelProperty(name = "name", dataType = "String", value = "表名")
 	private String name;
 	/**
 	 * 备注，说明
 	 */
+	@ApiModelProperty(name = "remark", dataType = "String", value = "备注，说明备注，说明")
 	private String remark;
 	
 	public TableDTO(String name, String remark) {
 		this.name = name;
 		this.remark = remark;
 	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getRemark() {
-		return remark;
-	}
-
-	public void setRemark(String remark) {
-		this.remark = remark;
-	}
-
 
 }
