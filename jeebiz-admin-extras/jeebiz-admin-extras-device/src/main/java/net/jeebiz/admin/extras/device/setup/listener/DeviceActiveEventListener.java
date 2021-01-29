36b8976aa6fe3c6e190a53df45f129f1a2e8e26b
wandl <hiwepy@163.com>
2021-01-29 10:41:53 +0800

    设备激活

--- jeebiz-admin-extras/jeebiz-admin-extras-device/src/main/java/net/jeebiz/admin/extras/device/setup/listener/DeviceActiveEventListener.java
+++ jeebiz-admin-extras/jeebiz-admin-extras-device/src/main/java/net/jeebiz/admin/extras/device/setup/listener/DeviceActiveEventListener.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
 import net.jeebiz.admin.extras.device.dao.IDeviceActivateDao;
 import net.jeebiz.admin.extras.device.dao.entities.DeviceActivateModel;
 import net.jeebiz.admin.extras.device.web.dto.DeviceActiveEventDTO;
 
 @Component
+@Slf4j
 public class DeviceActiveEventListener implements ApplicationListener<DeviceActiveEvent> {
 
     @Autowired
@@ -22,18 +24,22 @@ public class DeviceActiveEventListener implements ApplicationListener<DeviceActi
     
     @Override
     public void onApplicationEvent(DeviceActiveEvent event) {
-    	DeviceActiveEventDTO bind = event.getBind();
-        DeviceActivateModel model = getDeviceActivationDao().getActivatedByDeviceIMEI(bind.getDeviceIMEI());
-        if(Objects.isNull(model)){
-        	DeviceActivateModel activationModel = DeviceActivateModel.builder()
-                    .appId(bind.getAppId())
-                    .appChannel(bind.getAppChannel())
-                    .appVersion(bind.getAppVersion())
-                    .deviceImei(bind.getDeviceIMEI())
-                    .deviceModel(bind.getDeviceModel())
-                    .build();
-        	getDeviceActivationDao().insert(activationModel);
-        }
+    	try {
+			DeviceActiveEventDTO bind = event.getBind();
+			DeviceActivateModel model = getDeviceActivationDao().getActivatedByDeviceIMEI(bind.getDeviceIMEI());
+			if(Objects.isNull(model)){
+				DeviceActivateModel activationModel = DeviceActivateModel.builder()
+			            .appId(bind.getAppId())
+			            .appChannel(bind.getAppChannel())
+			            .appVersion(bind.getAppVersion())
+			            .deviceImei(bind.getDeviceIMEI())
+			            .deviceModel(bind.getDeviceModel())
+			            .build();
+				getDeviceActivationDao().insert(activationModel);
+			}
+		} catch (Exception e) {
+			log.error("Device Activate Exception:", e);
+		}
     }
     
     public IDeviceActivateDao getDeviceActivationDao() {
--- jeebiz-admin-extras/jeebiz-admin-extras-device/src/main/java/net/jeebiz/admin/extras/device/setup/listener/DeviceBindEventListener.java
+++ jeebiz-admin-extras/jeebiz-admin-extras-device/src/main/java/net/jeebiz/admin/extras/device/setup/listener/DeviceBindEventListener.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 
+import lombok.extern.slf4j.Slf4j;
 import net.jeebiz.admin.extras.device.dao.IDeviceActivateDao;
 import net.jeebiz.admin.extras.device.dao.IDeviceBindDao;
 import net.jeebiz.admin.extras.device.dao.entities.DeviceActivateModel;
@@ -19,6 +20,7 @@ import net.jeebiz.admin.extras.device.dao.entities.DeviceBindModel;
 import net.jeebiz.admin.extras.device.web.dto.DeviceBindEventDTO;
 
 @Component
+@Slf4j
 public class DeviceBindEventListener implements ApplicationListener<DeviceBindEvent> {
 
 
@@ -30,33 +32,37 @@ public class DeviceBindEventListener implements ApplicationListener<DeviceBindEv
     @Override
     public void onApplicationEvent(DeviceBindEvent event) {
     	
-    	// 1、根据参数查询对应的设备是否已经存在
-    	DeviceBindEventDTO bind = event.getBind();
-        DeviceActivateModel model = getDeviceActivationDao().getActivatedByDeviceIMEI(bind.getDeviceIMEI());
-        if(Objects.nonNull(model)){
-        	// 2、查询设备对应的用户绑定数据
-        	DeviceBindModel bindModel = getDeviceBindDao().selectOne(new QueryWrapper<DeviceBindModel>()
-        			.eq("ACTIVATED_ID", model.getId())
-        			.eq("CREATOR", bind.getUid()));
-        	// 2.1、存在则更新客户端信息
-        	if(Objects.nonNull(bindModel)){
-        		bindModel.setAppId(bind.getAppId());
-        		bindModel.setAppChannel(bind.getAppChannel());
-        		bindModel.setAppVersion(bind.getAppVersion());
-        		getDeviceBindDao().updateById(bindModel);
-        	} 
-        	// 2.2、不存在，则新增记录
-        	else {
-        		bindModel = DeviceBindModel.builder()
-    				 .activatedId(model.getId())
-                     .appId(bind.getAppId())
-                     .appChannel(bind.getAppChannel())
-                     .appVersion(bind.getAppVersion())
-                     .creator(bind.getUid())
-                     .build();
-        		getDeviceBindDao().insert(bindModel);
-        	}
-        }
+    	try {
+			// 1、根据参数查询对应的设备是否已经存在
+			DeviceBindEventDTO bind = event.getBind();
+			DeviceActivateModel model = getDeviceActivationDao().getActivatedByDeviceIMEI(bind.getDeviceIMEI());
+			if(Objects.nonNull(model)){
+				// 2、查询设备对应的用户绑定数据
+				DeviceBindModel bindModel = getDeviceBindDao().selectOne(new QueryWrapper<DeviceBindModel>()
+						.eq("ACTIVATED_ID", model.getId())
+						.eq("CREATOR", bind.getUid()));
+				// 2.1、存在则更新客户端信息
+				if(Objects.nonNull(bindModel)){
+					bindModel.setAppId(bind.getAppId());
+					bindModel.setAppChannel(bind.getAppChannel());
+					bindModel.setAppVersion(bind.getAppVersion());
+					getDeviceBindDao().updateById(bindModel);
+				} 
+				// 2.2、不存在，则新增记录
+				else {
+					bindModel = DeviceBindModel.builder()
+						 .activatedId(model.getId())
+			             .appId(bind.getAppId())
+			             .appChannel(bind.getAppChannel())
+			             .appVersion(bind.getAppVersion())
+			             .creator(bind.getUid())
+			             .build();
+					getDeviceBindDao().insert(bindModel);
+				}
+			}
+    	} catch (Exception e) {
+			log.error("Device Bind Exception:", e);
+		}
     }
     
     public IDeviceActivateDao getDeviceActivationDao() {
