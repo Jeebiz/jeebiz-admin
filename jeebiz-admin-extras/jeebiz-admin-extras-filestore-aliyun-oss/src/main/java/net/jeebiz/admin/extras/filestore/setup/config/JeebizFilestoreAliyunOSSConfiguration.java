7939484f0706b4d7971237a08565d1059ec71dd2
wandl <hnxxyhcwdl1003@163.com>
2021-07-02 00:29:37 +0800

    阿里云oss实现

---
+++ jeebiz-admin-extras/jeebiz-admin-extras-filestore-aliyun-oss/src/main/java/net/jeebiz/admin/extras/filestore/setup/config/JeebizFilestoreAliyunOSSConfiguration.java
@@ -0,0 +1,49 @@
+/** 
+ * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
+ * All Rights Reserved. 
+ */
+package net.jeebiz.admin.extras.filestore.setup.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.cloud.spring.boot.oss.OssConstants;
+import com.aliyun.oss.OSS;
+
+import net.jeebiz.admin.extras.filestore.dao.IFileMapper;
+import net.jeebiz.admin.extras.filestore.setup.provider.AliyunOssFilestoreProvider;
+import net.jeebiz.admin.extras.filestore.setup.provider.AliyunOssTemplate;
+import net.jeebiz.admin.extras.filestore.setup.provider.FilestoreProvider;
+
+/**
+ * 存储目标（local:服务本地,fdfs:FastDFS存储服务,minio:MinIO对象存储）
+ */
+@AutoConfigureAfter(name = {"com.alibaba.cloud.spring.boot.oss.autoconfigure.OssContextAutoConfiguration"})
+@Configuration
+@ConditionalOnProperty(name = OssConstants.ENABLED, havingValue = "true", matchIfMissing = true)
+@EnableConfigurationProperties({ AliyunOssProperties.class })
+public class JeebizFilestoreAliyunOSSConfiguration {
+
+	@Bean
+	public AliyunOssFilestoreProvider aliyunOssFilestoreProvider(IFileMapper fileMapper, OSS ossClient,
+			AliyunOssProperties ossProperties, AliyunOssTemplate ossTemplate) {
+		return new AliyunOssFilestoreProvider(fileMapper, ossClient, ossProperties, ossTemplate);
+	}
+	
+	@Bean
+	public AliyunOssTemplate aliyunOssTemplate(OSS ossClient) {
+		return new AliyunOssTemplate(ossClient);
+	}
+	
+	@Bean
+	public FilestoreProvider filestoreProvider(IFileMapper fileMapper, 
+			OSS ossClient,
+			AliyunOssProperties ossProperties, 
+			AliyunOssTemplate ossTemplate) {
+		return new AliyunOssFilestoreProvider(fileMapper, ossClient, ossProperties, ossTemplate);
+	}
+	
+}
---
+++ jeebiz-admin-extras/jeebiz-admin-extras-filestore-aliyun-oss/src/main/java/net/jeebiz/admin/extras/filestore/setup/provider/AliyunOssTemplate.java
@@ -0,0 +1,44 @@
+/** 
+ * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
+ * All Rights Reserved. 
+ */
+package net.jeebiz.admin.extras.filestore.setup.provider;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.aliyun.oss.OSS;
+
+public class AliyunOssTemplate {
+	
+	private OSS ossClient;
+	
+	public AliyunOssTemplate(OSS ossClient) {
+		this.ossClient = ossClient;
+	}
+
+	public String getAccsssURL(String bucket, String path) throws Exception {
+		// 过期时间为当日23:59:59
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DAY_OF_YEAR, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Date expiration = new Date(cal.getTimeInMillis());
+		return getOssClient().generatePresignedUrl(bucket, path, expiration).toString();
+	}
+	
+	public String getAccsssURL(AliyunOssStorePath storePath) throws Exception {
+		return this.getAccsssURL(storePath.getBucket(), storePath.getPath());
+	}
+	
+	public String getThumbAccsssURL(AliyunOssStorePath storePath) throws Exception {
+		return this.getAccsssURL(storePath.getBucket(), storePath.getThumb());
+	}
+	
+	public OSS getOssClient() {
+		return ossClient;
+	}
+
+}
