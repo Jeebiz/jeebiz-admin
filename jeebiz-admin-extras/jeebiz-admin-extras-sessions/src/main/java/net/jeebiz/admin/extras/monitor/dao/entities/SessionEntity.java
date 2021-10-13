9607b55d8061749b9e1f57cd2287ff904d90b66d
wandl <hnxxyhcwdl1003@163.com>
2021-10-14 00:38:42 +0800

    数据字典

--- jeebiz-admin-extras/jeebiz-admin-extras-sessions/src/main/java/net/jeebiz/admin/extras/monitor/dao/entities/SessionEntity.java
+++ jeebiz-admin-extras/jeebiz-admin-extras-sessions/src/main/java/net/jeebiz/admin/extras/monitor/dao/entities/SessionEntity.java
@@ -5,6 +5,7 @@
 package net.jeebiz.admin.extras.monitor.dao.entities;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.ibatis.type.Alias;
 import org.apache.shiro.biz.web.Constants;
@@ -26,7 +27,7 @@ import lombok.NoArgsConstructor;
 import net.jeebiz.boot.api.dao.entities.PaginationEntity;
 
 @SuppressWarnings("serial")
-@TableName(value = "sys_authz_user_sessions")
+@TableName(value = "sys_authz_session")
 @Builder
 @Data
 @NoArgsConstructor
@@ -53,10 +54,10 @@ public class SessionEntity extends PaginationEntity<SessionEntity> {
 	private String host;
 	/** 用户登录时间 */
 	@TableField(value = "s_start_timestamp")
-	private String startTimestamp;
+	private Date startTimestamp;
 	/** 最后访问时间 */
 	@TableField(value = "s_last_access_time")
-	private String lastAccessTime;
+	private Date lastAccessTime;
 	/** 用户浏览器类型 */
 	@TableField(value = "s_ua")
 	private String userAgent;
@@ -68,14 +69,14 @@ public class SessionEntity extends PaginationEntity<SessionEntity> {
 	private String forceLogout;
 	/** 备份的当前用户会话 */
 	@TableField(value = "s_session")
-    private OnlineSession session;
+    private String session;
 	/** 会话多久后过期（毫秒） */
 	@TableField(value = "s_timeout")
 	private long timeout;
 	/** 在线状态（0：离线，1：在线） */
 	@TableField(value = "s_status")
 	private String status;
-
+/*
 	public SessionEntity(String sessionId, String host, String startTimestamp, String lastAccessTime,
 			long timeout) {
 		this.sessionId = sessionId;
@@ -98,6 +99,6 @@ public class SessionEntity extends PaginationEntity<SessionEntity> {
 		if(Boolean.TRUE.equals(session.getAttribute(Constants.SESSION_FORCE_LOGOUT_KEY))) {
 			this.status =  OnlineStatus.FORCE_LOGOUT.getInfo();
 		}
-	}
+	}*/
 
 }
