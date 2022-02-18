27fbc33e33309dc00a7a5ec8bda9053d70fa9da7
wandl <hnxxyhcwdl1003@163.com>
2022-02-18 23:50:05 +0800

    优化

--- jeebiz-admin-extras/jeebiz-admin-extras-banner/src/main/resources/db/migration/banner/mysql/V1_0_1__横幅配置-结构初始化.sql
+++ jeebiz-admin-extras/jeebiz-admin-extras-banner/src/main/resources/db/migration/banner/mysql/V1_0_1__横幅配置-结构初始化.sql
@@ -1,22 +1,27 @@
 
 -- ----------------------------
--- Table structure for 我的应用信息表
+-- Table structure for 横幅广告配置表
 -- ----------------------------
-DROP TABLE IF EXISTS `sys_data_myapp`;
-CREATE TABLE `sys_data_bannermyapp` (
-  `app_id` bigint(12) NOT NULL AUTO_INCREMENT COMMENT '应用ID编号',
-  `app_uid` varchar(50) NOT NULL COMMENT '应用UID',
-  `app_name` varchar(100) DEFAULT NULL COMMENT '应用名称',
-  `app_intro` varchar(2000) DEFAULT NULL COMMENT '应用描述',
-  `app_lang` varchar(50) DEFAULT NULL COMMENT '应用开发语言',
-  `app_addr` varchar(500) DEFAULT NULL COMMENT '应用部署地址',
-  `app_key` varchar(100) COMMENT '应用Key',
-  `app_secret` varchar(500) COMMENT '应用Secret',
-  `app_userid` varchar(32) DEFAULT NULL COMMENT '应用所属人ID',
+DROP TABLE IF EXISTS `sys_data_banner`;
+CREATE TABLE `sys_data_banner` (
+  `id` bigint(12) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `app_id` varchar(50) DEFAULT NULL COMMENT '客户端应用ID',
+  `app_channel` varchar(200) DEFAULT NULL COMMENT '客户端应用渠道编码；多个编码使用,拼接',
+  `region_code` varchar(300) DEFAULT NULL COMMENT '地区编码；多个编码使用,拼接',
+  `language` varchar(30) DEFAULT NULL COMMENT '语区标签；多个语区使用,拼接',
+  `icon_url` varchar(255) DEFAULT NULL COMMENT '横幅图标',
+  `banner_url` varchar(255) DEFAULT NULL COMMENT '横幅图片',
+  `banner_desc` text DEFAULT NULL COMMENT '该横幅对应的描述',
+  `jump_url` varchar(255) DEFAULT NULL COMMENT '跳转路径',
+  `type` tinyint(4) DEFAULT NULL COMMENT '横幅类型（0：首页轮班、1：我的页面轮播、2：搜索页面轮播）',
+  `status` tinyint(4) DEFAULT NULL COMMENT '显示状态（0：不显示、1：显示）',
+  `link_type` tinyint(3) DEFAULT NULL COMMENT '链接类型（0:H5网页、1:客户端、2:仅图片、3:内联网页）',
+  `extend` varchar(255) DEFAULT NULL COMMENT '扩展字段：过期时间、等待时间',
+  `priority` int(11) DEFAULT '999' COMMENT '权重 越小越靠前',
   `is_delete` tinyint(2) NOT NULL DEFAULT '0' COMMENT '是否删除（0:未删除,1:已删除）',
   `creator` bigint(12) DEFAULT '0' COMMENT '创建人ID',
   `create_time` timestamp DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `modifyer` bigint(12) DEFAULT NULL COMMENT '修改人ID',
   `modify_time` timestamp DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  PRIMARY KEY (`app_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='我的应用信息表';
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='横幅广告配置表';
--- jeebiz-admin-extras/jeebiz-admin-extras-banner/src/main/resources/db/migration/banner/oracle/V1_0_1__横幅配置-结构初始化.sql
+++ jeebiz-admin-extras/jeebiz-admin-extras-banner/src/main/resources/db/migration/banner/oracle/V1_0_1__横幅配置-结构初始化.sql
@@ -1,38 +1,42 @@
 
 -- ----------------------------
--- Table structure for sys_data_myapp
+-- Table structure for sys_data_banner
 -- ----------------------------
-create table sys_data_myapp(
-  	app_id     	VARCHAR2(32) default sys_guid() not null,
-  	app_uid		VARCHAR2(50) not null,
-  	app_name   	VARCHAR2(100) not null,
-  	app_intro 	VARCHAR2(2000),
-  	app_lang	VARCHAR2(50),
-  	app_addr	VARCHAR2(500),
-    app_key	    VARCHAR2(100),
-    app_secret	VARCHAR2(500),
-  	app_userid 	VARCHAR2(32) not null,
-    is_delete		VARCHAR2(2),
-    creator			VARCHAR2(32),
-    create_time		VARCHAR2(32) default to_char(sysdate ,'yyyy-mm-dd hh24:mi:ss'),
-    modifyer		VARCHAR2(32),
-    modify_time		VARCHAR2(32),
+create table sys_data_banner(
+  	id     	    varchar(32) default sys_guid() not null,
+    app_id      varchar(50) DEFAULT NULL COMMENT '客户端应用ID',
+    app_channel varchar(20) DEFAULT NULL COMMENT '客户端应用渠道编码',
+    region_code varchar(30) DEFAULT NULL COMMENT '地区编码',
+
+  	app_uid		varchar(50) not null,
+  	app_name   	varchar(100) not null,
+  	app_intro 	varchar(2000),
+  	app_lang	varchar(50),
+  	app_addr	varchar(500),
+    app_key	    varchar(100),
+    app_secret	varchar(500),
+  	app_userid 	varchar(32) not null,
+    is_delete		varchar(2),
+    creator			varchar(32),
+    create_time		varchar(32) default to_char(sysdate ,'yyyy-mm-dd hh24:mi:ss'),
+    modifyer		varchar(32),
+    modify_time		varchar(32),
   	PRIMARY KEY (`app_id`)
 );
 -- Add comments to the table
-comment on table sys_data_myapp is '我的应用信息表';
+comment on table sys_data_banner is '我的应用信息表';
 -- Add comments to the columns
-comment on column sys_data_myapp.app_id is '应用ID编号';
-comment on column sys_data_myapp.app_uid  is '应用UID';
-comment on column sys_data_myapp.app_name  is '应用名称';
-comment on column sys_data_myapp.app_intro  is '应用描述';
-comment on column sys_data_myapp.app_lang  is '应用开发语言';
-comment on column sys_data_myapp.app_addr  is '应用部署地址';
-comment on column sys_data_myapp.app_key  is '应用Key';
-comment on column sys_data_myapp.app_secret  is '应用Secret';
-comment on column sys_data_myapp.app_userid  is '应用所属人ID';
-comment on column sys_data_myapp.is_delete  is '是否删除（0：未删除，1：已删除）';
-comment on column sys_data_myapp.creator  is '创建人ID';
-comment on column sys_data_myapp.create_time  is '创建时间';
-comment on column sys_data_myapp.modifyer  is '修改人ID';
-comment on column sys_data_myapp.modify_time  is '修改时间';
+comment on column sys_data_banner.app_id is '主键ID';
+comment on column sys_data_banner.app_uid  is '应用UID';
+comment on column sys_data_banner.app_name  is '应用名称';
+comment on column sys_data_banner.app_intro  is '应用描述';
+comment on column sys_data_banner.app_lang  is '应用开发语言';
+comment on column sys_data_banner.app_addr  is '应用部署地址';
+comment on column sys_data_banner.app_key  is '应用Key';
+comment on column sys_data_banner.app_secret  is '应用Secret';
+comment on column sys_data_banner.app_userid  is '应用所属人ID';
+comment on column sys_data_banner.is_delete  is '是否删除（0：未删除，1：已删除）';
+comment on column sys_data_banner.creator  is '创建人ID';
+comment on column sys_data_banner.create_time  is '创建时间';
+comment on column sys_data_banner.modifyer  is '修改人ID';
+comment on column sys_data_banner.modify_time  is '修改时间';
